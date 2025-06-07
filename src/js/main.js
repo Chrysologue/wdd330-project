f1890599078a86f3541de720517a7d7263cb9ffe
Chrysologue <ChrysoBeatz@gmail.com>
2025-06-07 16:21:43 +0300

    Navigation menu workout

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log("hello")
+import {
+  setupToggleMenu,
+  setupResizeHandler,
+  setupLoadHandler,
+} from "./utils.mjs";
+
+const header = document.querySelector(".header");
+const hamburger = document.querySelector(".hamburger-lines");
+const navLinks = document.querySelector(".navItems");
+const body = document.querySelector("body");
+
+setupToggleMenu(header, hamburger, navLinks, body);
+setupResizeHandler(header, navLinks, body);
+setupLoadHandler(header, navLinks, body);
